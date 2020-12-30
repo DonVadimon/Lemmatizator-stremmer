301b3ead9b4935a4d48ba8fbce489b95289d7436
Vadim Khizhnyakov <provcydia@gmail.com>
2020-12-30 21:48:30 +0300

    filesystem dont want to include

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include "Lemmatizator.h"
 
 int main()
 {
-    std::cout << "\nHello world!\n";
+    auto lemms = Lemmatizator::lemmatize("../stest.txt");
+    for (auto lemm : lemms)
+    {
+        std::wcout << "_" << lemm.first << "_" << " - " << lemm.second << '\n';
+    }
+    
+    std::cout << "\n\n\n\n";
+    
     return 0;
 }
