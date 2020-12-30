304adddeb2988212b3e7a32372a8543e24061a77
Vadim Khizhnyakov <provcydia@gmail.com>
2020-12-31 00:40:26 +0300

    fix test

--- test/test_Lemmatizator.cpp
+++ test/test_Lemmatizator.cpp
@@ -1,6 +1,5 @@
 #include "gtest/gtest.h"
 #include <filesystem>
-#include <fstream>
 #include <string>
 #include "Lemmatizator.h"
 
@@ -8,14 +7,7 @@ namespace fs = std::filesystem;
 
 TEST(lemmatize, onLemm)
 {
-    std::fstream fileDescriptor;
     std::string filePath = "test.txt";
-    fileDescriptor.open(filePath, std::ios_base::out);
-    fileDescriptor << "readed readed readed readed readed readed readed readed "
-                      "reading reading reading reading reading "
-                      "reads reads reads reads reads "
-                      "readors readers readens ";
-    fileDescriptor.close();
     auto lemm = Lemmatizator::lemmatize(filePath);
     EXPECT_EQ(lemm.size(), 2);
     size_t count = lemm.find(L"read")->second;
